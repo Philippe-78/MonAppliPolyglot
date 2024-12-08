8e715ccfb84fe6980f4cbcb9b08de305258ef8f7
Philippe CHARLES <A114SR@login.axa>
2024-12-08 16:54:32 +0100

    route / corrigée

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/Home" element={<PageHome/>} />
+        <Route path="/" element={<PageHome/>} />
         <Route path="/ajouter" element={<PageAjouter/>} />
         <Route path="/consulter" element={<PageConsulter />} />
       </Routes>
